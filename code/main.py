560a48caf92b4f290de236105a27822b80021321
Anaëlle ROBIN <anaelle.robin@cpe.fr>
2025-05-20 09:32:59 +0200

    Q_10 : done

--- code/main.py
+++ code/main.py
@@ -3,6 +3,7 @@
 import OpenGL.GL as GL
 import glfw
 import numpy as np
+import tools 
 
 class Game(object):
     """ fenêtre GLFW avec openGL """
@@ -45,6 +46,9 @@ class Game(object):
         GL.glEnable(GL.GL_DEPTH_TEST)
 
     def init_programs(self):
+        program = tools.create_program_from_file("code/shader.vert", "code/shader.frag")
+        GL.glUseProgram(program)
+
         pass
         
     def init_data(self):
@@ -75,48 +79,6 @@ class Game(object):
         if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
             glfw.set_window_should_close(win, glfw.TRUE)
 
-    def compile_shader(shader_content, shader_type):
-        # compilation d'un shader donné selon son type
-        shader_id = GL.glCreateShader(shader_type)
-        GL.glShaderSource(shader_id, shader_content)
-        GL.glCompileShader(shader_id)
-
-        success = GL.glGetShaderiv(shader_id, GL.GL_COMPILE_STATUS)
-        if not success:
-            log = GL.glGetShaderInfoLog(shader_id).decode('ascii')
-            print(f'{25*"-"}\nError compiling shader: \n{shader_content}\n{5*"-"}\n{log}\n{25*"-"}')
-        return shader_id
-
-    def create_program(vertex_source, fragment_source):
-        # création d'un programme GPU
-        vs_id = compile_shader(vertex_source, GL.GL_VERTEX_SHADER)
-        fs_id = compile_shader(fragment_source, GL.GL_FRAGMENT_SHADER)
-
-        if vs_id and fs_id:
-            program_id = GL.glCreateProgram()
-            GL.glAttachShader(program_id, vs_id)
-            GL.glAttachShader(program_id, fs_id)
-            GL.glLinkProgram(program_id)
-
-            success = GL.glGetProgramiv(program_id, GL.GL_LINK_STATUS)
-            if not success:
-                log = GL.glGetProgramInfoLog(program_id).decode('ascii')
-                print(f'{25*"-"}\nError linking program:\n{log}\n{25*"-"}')
-
-            GL.glDeleteShader(vs_id)
-            GL.glDeleteShader(fs_id)
-
-            return program_id
-
-    def create_program_from_file(vs_file, fs_file):
-        # création d'un programme GPU à partir de fichiers
-        vs_content = open(vs_file, 'r').read() if os.path.exists(vs_file) \
-            else print(f'{25*"-"}\nError reading file:\n{vs_file}\n{25*"-"}')
-
-        fs_content = open(fs_file, 'r').read() if os.path.exists(fs_file) \
-            else print(f'{25*"-"}\nError reading file:\n{fs_file}\n{25*"-"}')
-
-        return create_program(vs_content, fs_content)
 
 def main():
     g = Game()
